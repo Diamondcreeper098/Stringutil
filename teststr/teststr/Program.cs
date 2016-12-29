442b905f1c9f86be011ff4a49327a74c4c0b805d
Diamondcreeper098 <dmnd098@gmail.com>
2016-12-29 20:00:50 +0330

    Add files via upload

---
+++ teststr/teststr/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace teststr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A thing
+            string str = "Hello Wo rl d";
+            string str1 = "asd";
+            StringUtil stru = new StringUtil(str);
+            StringUtil stru1 = new StringUtil(str1);
+            string anotherstr = stru.Reverse();
+            Console.WriteLine("Text is: {0}",str);
+            Console.WriteLine("Reversed Text is: {0}", anotherstr);
+            Console.WriteLine("str: Count is : {0} Spaces: {1} Without Space: {2}",stru.Countchars(),stru.Countspaces(),stru.Countwithoutspaces());
+            Console.WriteLine("str1: Count is : {0} Spaces: {1} Without Space: {2}", stru1.Countchars(), stru1.Countspaces(), stru1.Countwithoutspaces());
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ teststr/teststr/StringUtil.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace teststr
+{
+    class StringUtil
+    {
+        public string txt;
+        public StringUtil(string str)
+        {
+            txt = str;
+        }
+        public StringUtil()
+        { }
+        public string Reverse()
+        {
+            string res="";
+            char[] txtt = txt.ToCharArray();
+            for (int i = txtt.Length - 1; i >= 0; i--)
+            {
+                res += txtt[i];
+            }
+            return res;
+        }
+        public string Reverse(string text)
+        {
+            string res = "";
+            char[] txtt = text.ToCharArray();
+            for (int i = txtt.Length - 1; i >= 0; i--)
+            {
+                res += txtt[i];
+            }
+            return res;
+        }
+        public int Countchars()
+        {
+            
+            return txt.ToCharArray().Length;
+        }
+        public int Countchars(string text)
+        {
+
+            return text.ToCharArray().Length;
+        }
+        public int Countspaces()
+        {
+            int r = 0;
+            char[] tt = txt.ToCharArray();
+            foreach (var item in tt)
+            {
+                if (item == ' ')
+                {
+                    r++;
+                }
+            }
+            return r;
+        }
+        public int Countspaces(string text)
+        {
+            int r = 0;
+            char[] tt = text.ToCharArray();
+            foreach (var item in tt)
+            {
+                if (item == ' ')
+                {
+                    r++;
+                }
+            }
+            return r;
+        }
+        public int Countwithoutspaces()
+        {
+            int r = 0;
+            char[] tt = txt.ToCharArray();
+            foreach (var item in tt)
+            {
+                if (item != ' ')
+                {
+                    r++;
+                }
+            }
+            return r;
+        }
+        public int Countwithoutspaces(string text)
+        {
+            int r = 0;
+            char[] tt = text.ToCharArray();
+            foreach (var item in tt)
+            {
+                if (item != ' ')
+                {
+                    r++;
+                }
+            }
+            return r;
+        }
+    }
+}
